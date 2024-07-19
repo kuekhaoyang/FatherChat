730ba525a87a59807d930afa518d93858d57f38f
fatherxtreme123 <thunderstrikelord@gmail.com>
2024-07-19 08:40:24 +0800

    new file:   assets/stop.svg modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   
     const openAIModels = [
-        'gpt-4o', 'gpt-4o-2024-05-13', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09',
+        'gpt-4o-mini', 'gpt-4o', 'gpt-4o-2024-05-13', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09',
         'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-1106-preview',
         'gpt-4-vision-preview', 'gpt-4-1106-vision-preview', 'gpt-4',
         'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613', 'gpt-3.5-turbo-0125',
