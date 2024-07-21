54c9dbe096343ee1b5f9bb31c662e15292866203
fatherxtreme123 <thunderstrikelord@gmail.com>
2024-07-21 16:43:58 +0800

    Update model list

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'model-provider': 'openai',
         'api-key': '',
         'api-host': 'https://api.openai.com/v1',
-        'model': 'gpt-4o',
+        'model': 'gpt-4o-mini-2024-07-18',
         'temperature': 0.7,
         'top-p': 1.0,
         'presence-penalty': 0.0,
@@ -37,17 +37,16 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   
     const openAIModels = [
-        'gpt-4o-mini', 'gpt-4o', 'gpt-4o-2024-05-13', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09',
-        'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-1106-preview',
-        'gpt-4-vision-preview', 'gpt-4-1106-vision-preview', 'gpt-4',
-        'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613', 'gpt-3.5-turbo-0125',
-        'gpt-3.5-turbo', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-instruct',
+        'gpt-4o-mini-2024-07-18', 'gpt-4o-mini', 'gpt-4o', 'gpt-4o-2024-05-13', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09',
+        'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-1106-preview', 'gpt-4-vision-preview',
+        'gpt-4-1106-vision-preview', 'gpt-4', 'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613',
+        'gpt-3.5-turbo-0125', 'gpt-3.5-turbo', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-instruct',
         'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613'
     ];
   
     const groqModels = [
-        'llama3-8b-8192', 'llama3-70b-8192', 'mixtral-8x7b-32768',
-        'gemma-7b-it', 'gemma2-9b-it'
+            'llama3-8b-8192', 'llama3-70b-8192', 'llama3-groq-8b-8192-tool-use-preview',
+            'llama3-groq-70b-8192-tool-use-preview', 'mixtral-8x7b-32768', 'gemma-7b-it', 'gemma2-9b-it'
     ];
   
 
